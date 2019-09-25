639774dce4ac4271805746b922608024b181e8d5
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2019-09-24 23:08:58 -0500

    Ejemplo de prueba para integracion con Jenkins

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.listen(process.env.PORT || 3000);
+console.log('listen in 3000');
+
+module.exports = app;
